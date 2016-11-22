76b56a934cdd180737cbd9541471e659c38872d7
kalisettys <kalisettys@vcu.edu>
2016-11-22 18:49:22 -0500

    Add files via upload

--- CommandLineInterface.java
+++ CommandLineInterface.java
@@ -1,3 +1,46 @@
 public class CommandLineInterface {
-	
-}
+
+	    public void proc() {
+
+	    }
+
+	    public void mem() {
+
+	    }
+
+	    public void load() {	      
+	    
+	    }
+
+	    public void exe() {
+	    
+	    }
+
+	    public void reset() {
+
+	    }
+	    
+	    public void exit() {
+	      
+	    }
+
+	    public void promptUser(){
+
+	    }
+	}
+
+
+
+
+
+//CommandLineInterface enables the user to type commands in a terminal or console window to interact with the OS.
+
+//COMMANDS
+/*proc()
+mem()
+load()
+exe()
+reset()
+promptUser()
+*/
+
---
+++ Memory.java
@@ -0,0 +1,46 @@
+
+public class Memory {
+
+    public int memTotal;
+    public int memUsed = 0;
+
+    public Memory(int mem) {
+        memTotal = mem;
+    }
+
+    public Memory() {
+
+    }
+
+    public void setFullMemory(int value) {
+        memTotal = value;
+    }
+
+    public int addMemUsed(int value) {
+        memUsed = memUsed + value;
+        return memUsed;
+    }
+
+    public int subtractMemUsed(int value) {
+        memUsed = memUsed - value;
+        if(memUsed < 0)
+            memUsed = 0;
+        return memUsed;
+    }
+
+    public int getFullMemory() {
+        return memTotal;
+    }
+
+    public int getMemoryUsed() {
+        return memUsed;
+    }
+
+    public int getAvailableMemory() {
+        return memTotal - memUsed;
+    }
+
+}
+
+
+//
