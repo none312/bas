be31579938eba47739cbd17f778733ea14a2e863
kalisettys <kalisettys@vcu.edu>
2016-11-25 20:54:17 -0500

    Update Parsing.java

--- Parsing.java
+++ Parsing.java
@@ -3,46 +3,47 @@ import java.io.*;
 
 public class Parsing {
 
-    public String inputfile = "input.txt";
-    public PriorityQueue<String> queue = new PriorityQueue<String>();
-    public File file = new File(inputfile);
-    public Scanner input;
-    public String command, value;
-
+    private String readFile;
+    private String cmd, val;
+    private PriorityQueue<String> queue = new PriorityQueue<String>();
+    private Scanner input;
+    
     public Parsing() {
     }
     public void parseFile(String filename){
-        this.inputfile = filename;
-        parse();
+        this.readFile = "executions\\"+filename;
+       System.out.println(readFile);
+       parseFile();
     }
     public void parseLine(String command) {
-      command.toLowerCase();
-      input = new Scanner(command);
-      command = input.next();
-      value = null;
-      if(input.hasNext())
-        value = input.next();
-      input.close();
+     command = command.toLowerCase();
+     input = new Scanner(command);
+     this.cmd = input.next();
+     val = null;
+     if(input.hasNext())
+    	 val = input.next();
+     input.close();
     }
 
-    private void parse() {
+    private void parseFile() {
         try {
-            input = new Scanner(file);
+        	File sampleFile = new File(readFile);
+            input = new Scanner(sampleFile);
             while(input.hasNext()) {
                 queue.add(input.next());
             }
-        } catch (Exception e) { System.out.println("__________"); }
+        } catch (Exception e) { System.out.println("FILE NOT FOUND EXCEPTION"); }
         input.close();
     }
 
-    public PriorityQueue<String> getStuff() {
+    public PriorityQueue<String> getQueue() {
       return queue;
     }
     
-    public void testprint() {
-
-        while(queue.peek() != null) {
-            System.out.println(queue.poll());
+    public String getcmd() {
+    	return cmd;
+    }
+     public String getval(){
+    	 return val;
         }
     }
-}
