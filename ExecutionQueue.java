af20eae6371ee2b3b87ddceb7204e072826aaf42
kalisettys <kalisettys@vcu.edu>
2016-11-25 21:25:35 -0500

    are we done?

--- ExecutionQueue.java
+++ ExecutionQueue.java
@@ -1,9 +1,11 @@
 import java.util.PriorityQueue;
 
 public class ExecutionQueue {
+
     private PriorityQueue<Events> queue = new PriorityQueue<>();
 
     public void enQueue(Events event) {
+    	
         queue.add(event);
     }
 
@@ -11,3 +13,12 @@ public class ExecutionQueue {
         return queue.remove(0);
     }
 }
+
+ 
+//TODO: initiate ready queue, waiting queue and IO queue// or is this already done?
+
+//initialize int (availableMemory = 256)
+//PriorityQueue <Events> Ready queue = new PriorityQueue<>();
+//PriorityQueue <Events> Waiting queue = new PriorityQueue<>();
+//PriorityQueue <Events> IO queue = new PriorityQueue<>();
+
