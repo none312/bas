1ec062d3b5a15e11ddfcd62485440b0cdd9bd3ad
kalisettys <kalisettys@vcu.edu>
2016-11-25 22:16:14 -0500

    new additions

--- Gui.java
+++ Gui.java
@@ -2,8 +2,21 @@ import java.util.PriorityQueue;
 import java.awt.FlowLayout; //import default layout managing(ordering)
 import javax.swing.JFrame; // basic windows feature(title bar, minimize/maximize)
 import javax.swing.JLabel; // ouput text+images on screen
+import java.util.Vector;
+import java.util.NoSuchElementException;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.filechooser.*;
+import java.io.*;
+import javax.swing.*;
+import javax.swing.filechooser.*;
+import java.awt.Color;
+import java.awt.geom.*;
+import java.awt.image.*;
 
-public class Gui extends JFrame {
+public class Gui extends JFrame implements ActionListener {
 	 
 	private JLabel item1;
 	//constructor
