927e288e7898c5f177e501e794e1c1d95740636d
kalisettys <kalisettys@vcu.edu>
2016-11-26 21:27:37 -0500

    added comments and fixed error

--- EventQueue.java
+++ EventQueue.java
@@ -1,14 +1,17 @@
 import java.util.PriorityQueue;
 
+// Event queue is used by the interrupt system
+// EventQueue = priority queue (Binary Heap Structure)
+
 public class EventQueue {
-	private PriorityQueue<Events> queue = new PriorityQueue<>();
-	
-	public void enQueue(Events event) {
-	    queue.add(event);
-	} 
-	public boolean deQueue() {
-			return queue.remove(0);
-		}
+	PriorityQueue<ECB> eventQueue = new PriorityQueue<ECB>();
+
+	public void enQueue(ECB e) {
+		eventQueue.add(e);
 	}
-
-
+	
+	public void deQueue(ECB e)
+	{
+		eventQueue.remove(e);
+	}
+}
