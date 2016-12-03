716611a497540e1b7647e38c26548e866f6a5446
kalisettys <kalisettys@vcu.edu>
2016-12-02 23:27:29 -0500

    Update IOScheduler.java

--- IOScheduler.java
+++ IOScheduler.java
@@ -6,12 +6,7 @@ import java.util.Iterator;
 public class IOScheduler {
 	public static PriorityQueue<Process> jobQueue = new PriorityQueue<Process>();
 	public IOScheduler() {
-	//	jobQueue = new PriorityQueue<Process>(size);
-		//Process p1 = new Process("Process 1", 0, 1, 6);
-		//Process p2 = new Process("Process 2", 4, 2, 2);
-		//jobQueue.add(p1);
-		//jobQueue.add(p2);
-	}
+	
 	
 	public int randomIOburstTime()
 	{
@@ -21,14 +16,7 @@ public class IOScheduler {
 	}
 	public static void main (String args [] )
 	{
-	//	IOScheduler cpu = new IOScheduler(2);
-//		Iterator<Process> it = jobQueue.iterator();
-//		while(it.hasNext())
-//		{
-//			System.out.println(it.next().toString());
-//		}
-	//	cpu.fcfs(jobQueue);
-	
+
 	}
 
 }
