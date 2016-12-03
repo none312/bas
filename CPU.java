aa0d4fe3a262735a1edfbe379624e6edcbc955fa
kalisettys <kalisettys@vcu.edu>
2016-12-02 23:26:51 -0500

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -8,11 +8,9 @@ public class CPU {
 		cpuTime = 0;
 	}
 	public void advanceClock() {
-		// if(interupted == false && preempted == false) //advance clock if
-		// process is not getting intereupted or preempted
-		//cpuTime = clock.getClock();
+	
 		cpuTime++;
-		// clock.getClock()++;
+		
 	}
 	
 	public int getClock()
